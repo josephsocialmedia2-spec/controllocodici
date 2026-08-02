f03198aef3ff05036860ee8a5ad893d2a1f5ca73
josephsocialmedia2-spec <joseph.socialmedia2@gmail.com>
2026-08-02 18:36:28 +0200

    Aggiunge diagnostica audio Windows

---
+++ f1-assistente-telefonico-offline/DIAGNOSTICA_AUDIO.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+if not exist ".venv\Scripts\python.exe" (
+    echo Esegui prima INSTALLA_E_AVVIA.bat
+    pause
+    exit /b 1
+)
+".venv\Scripts\python.exe" "%~dp0tools\list_devices.py"
+echo.
+pause
